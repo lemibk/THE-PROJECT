6f5f61e05e2304b77a684ccd47bbe69c754a30ef
lemibk <139050997+lemibk@users.noreply.github.com>
2023-11-04 21:26:16 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 public class Student extends Person{
    
-    private int classroom;
+     int classroom;
 
     public Student(String name,int age,boolean parent_permission, int classroom) {
         super(name, age,parent_permission);
